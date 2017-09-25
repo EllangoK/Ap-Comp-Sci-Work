a34a90e0df6e35fbbe1dfad6058e1a4cb808690a
Karun Ellango <ellangok@bxscience.edu>
2017-09-25 11:52:12 -0400

    Create TargetLogo.java
    Move to folder later

---
+++ TargetLogo.java
@@ -0,0 +1,22 @@
+import javax.swing.JApplet;
+import java.awt.*;
+import java.applet.*;
+import java.io.*;
+public class TargetLogo extends Applet
+{
+//-----------------------------------------------------------------
+// Draws a snowman.
+//-----------------------------------------------------------------
+public void paint (Graphics page)
+{
+final int MID = 150;
+final int TOP = 50;
+setBackground (Color.white);
+page.setColor (Color.red);
+page.fillOval (MID-50, TOP, 200, 200);
+page.setColor (Color.white);
+page.fillOval (MID-25, TOP+25, 150, 150);
+page.setColor (Color.red);
+page.fillOval (MID, TOP+50, 100, 100);
+}
+}
