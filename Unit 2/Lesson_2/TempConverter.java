a5521b91cf07a71a622d547d31cce25641cfe978
Karun Ellango <ellangok@bxscience.edu>
2017-09-28 10:38:59 -0400

    Update TempConverter.java

--- Unit 2/Lesson_2/TempConverter.java
+++ Unit 2/Lesson_2/TempConverter.java
@@ -1,20 +1,18 @@
 package Lesson_2;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class TempConverter {
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Please enter temperature in Fahrenheit : ");
-        double fahrenheit = Double.parseDouble(reader.readLine());
-        double celsius = (5.0/9.0)*(fahrenheit - 32);
+ public static void main(String[] args){
+  // TODO Auto-generated method stub
+  double f;
+  Scanner keyboard = new Scanner(System.in);
+  System.out.print("Please enter temperature in Fahrenheit : ");
+        f = keyboard.nextDouble();
+        double celsius = (5.0/9.0) * (f - 32);
         System.out.println("Temperature in Celsius is : "+celsius);
 
-	}
+ }
 
 }
